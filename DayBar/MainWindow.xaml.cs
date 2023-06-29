ecde227888e629df308ee789c0166bdd1122651b
Léo Peyronnet <leo.peyronnet@outlook.fr>
2023-06-29 19:19:25 +0200

    Added MainWindow

--- DayBar/MainWindow.xaml.cs
+++ DayBar/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace DayBar;
 /// <summary>
@@ -22,5 +23,41 @@ public partial class MainWindow : Window
 	public MainWindow()
 	{
 		InitializeComponent();
+		InitTimer(0*3600, 24*3600);
+	}
+
+	DispatcherTimer dispatcherTimer = new();
+	
+	private void InitTimer(int startHour, int endHour)
+	{
+		int c = 0;
+
+		// Get the current time
+		DateTime now = DateTime.Now;
+		// Get the start of the day
+		DateTime startOfDay = now.Date;
+		// Get the difference as a TimeSpan
+		TimeSpan elapsed = now - startOfDay;
+		// Get the total number of seconds
+		int seconds = (int)elapsed.TotalSeconds;
+		c = seconds * 100 / (endHour - startHour);
+
+		dispatcherTimer.Interval = TimeSpan.FromMinutes(1);
+		dispatcherTimer.Tick += (o, e) =>
+		{
+			if (c > 100)
+			{
+				c = 100;
+			}
+			myNotifyIcon.IconSource = new BitmapImage(new Uri($"pack://application:,,,/Assets/Icons/{c}.ico"));
+		};
+		if (c > 100)
+		{
+			c = 100;
+		}
+		myNotifyIcon.IconSource = new BitmapImage(new Uri($"pack://application:,,,/Assets/Icons/{c}.ico"));
+
+		dispatcherTimer.Start();
+	}
 	}
 }
